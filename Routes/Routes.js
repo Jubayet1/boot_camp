a25a93a0672b31b36f6d6bd5694aa421c184f302
Meherab <jubayet0106@gmail.com>
2022-06-17 04:16:22 +0600

    route added

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,37 +1,32 @@
 const express = require('express');
-const dotenv = require('dotenv');
-// const route = require('./Routes/Routes');
+const router = express.Route();
 
-dotenv.config({path: './Config/Config.env'})
-
-const port = process.env.PORT;
-const app = express()
 
 // show all bootcamps
-app.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({status: 'true', todo: "show all bootcamps"});
 })
 
 // show single bootcamps
-app.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.status(200).json({status: 'true', todo: `display ${req.params.id} bootcamp`});
 })
 
 // create a bootcamps
-app.post('/api/v1/bootcamps', (req, res) => {
+router.post('', (req, res) => {
     res.status(200).json({status: 'true', todo: "create new bootcamp"});
 })
 
 // update a bootcamps
-app.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.status(200).json({status: 'true', todo: `update ${req.params.id} bootcamp`});
 })
 
 // delete a bootcamps
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(200).json({status: 'true', todo: `delete ${req.params.id} bootcamp`});
 })
 
-app.listen(port, ()=> {
-    console.log(`listening on port ${port}`);
-});
+
+// exporting file 
+module.exports = router;
