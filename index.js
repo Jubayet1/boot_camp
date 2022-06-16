e406083896110d88b3622a32750235986676d3c7
Meherab <jubayet0106@gmail.com>
2022-06-16 16:09:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './Config/Config.env'})
+
+const port = process.env.PORT;
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("hello world!")
+})
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+});
